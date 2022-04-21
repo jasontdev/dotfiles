84d86b59ad4f16c3d5f5d643663a926106ad08c7
Jason T <jason@jasont.dev>
2022-04-21 10:29:20 +1000

    Enabled null-ls to use gofmt

--- nvim/init.lua
+++ nvim/init.lua
@@ -125,6 +125,7 @@ require("null-ls").setup({
         require("null-ls").builtins.formatting.stylua,
         require("null-ls").builtins.diagnostics.eslint,
         require("null-ls").builtins.completion.spell,
-        require("null-ls").builtins.formatting.prettier
+        require("null-ls").builtins.formatting.prettier,
+        require("null-ls").builtins.formatting.gofmt
     },
 })
